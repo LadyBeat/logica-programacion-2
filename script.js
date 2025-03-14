e4bd544697415cc878dfa33da91bb83c8d26d391
LadyBeat <vizaddd@gmail.com>
2025-03-13 19:28:29 -0600

    codigo comentado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function convertirTemperatura() {
     let celsius = document.getElementById('celsius').value;
     celsius = parseFloat(celsius);
+    //convertimos a float para que no haya problemas al hacer la operación
 
     if (isNaN(celsius)) {
         alert("Por favor, ingrese un número válido.");
@@ -11,5 +12,10 @@ function convertirTemperatura() {
     const kelvin = celsius + 273.15;
 
     document.getElementById('resultado').innerText = 
-        `Grados Kelvin: ${kelvin.toFixed(2)}\nGrados Fahrenheit: ${fahrenheit.toFixed(2)}`;
+    //usamos innerText para que no se muestren las etiquetas html
+    //usamos toFixed para que solo muestre dos decimales
+    //usamos \n para hacer un salto de línea
+    //usamos ` para poder concatenar variables con texto
+
+    `Grados Kelvin: ${kelvin.toFixed(2)}\nGrados Fahrenheit: ${fahrenheit.toFixed(2)}`;
 }
